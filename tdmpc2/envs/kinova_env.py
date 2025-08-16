4a671d347f97024c37679e43794d63e2562ee6e6
VishGit1234 <vishalschool1234@gmail.com>
2025-08-15 21:19:47 -0400

    add stack cube env

--- tdmpc2/envs/kinova_env.py
+++ tdmpc2/envs/kinova_env.py
@@ -9,16 +9,24 @@ def make_env(cfg):
   if cfg.task not in SUPPORTED_TASKS:
     raise ValueError('Unknown task:', cfg.task)
   kwargs = {}
-  if cfg.task == 'kinova_push_cube' or cfg.task == 'kinova_pick_cube':
+  if cfg.task == 'kinova_push_cube' or cfg.task == 'kinova_pick_cube' or cfg.task == 'kinova_stack_cube':
+    if cfg.task == 'kinova_push_cube':
+      task_name = "KinovaPushCube"
+      target_offset = cfg.push_target_offset
+    elif cfg.task == 'kinova_pick_cube':
+      task_name = "KinovaPickCube"
+      target_offset = cfg.pick_target_offset
+    elif cfg.task == 'kinova_stack_cube':
+      task_name = "KinovaStackCube"
+      target_offset = cfg.stack_target_offset
     # Init kwargs dict
     kwargs = {
       "block_offset": cfg.init_box_pos,
       "block_gen_range": cfg.box_gen_range,
-      "target_offset": cfg.push_target_offset if cfg.task == "kinova_push_cube" else cfg.pick_target_offset,
+      "target_offset": target_offset,
       "goal_radius": cfg.termination_if_cube_goal_dist_less_than,
       "cube_randomization_ranges": cfg.cube_randomization_ranges,
     }
-    task_name = 'KinovaPushCube' if cfg.task == 'kinova_push_cube' else 'KinovaPickCube'
     env = gym.make(
       task_name,
       num_envs=cfg.num_envs,
--- tdmpc2/envs/kinova_envs/__init__.py
+++ tdmpc2/envs/kinova_envs/__init__.py
@@ -1,9 +1,11 @@
 from envs.kinova_envs.KinovaAgent import KinovaGen3
 from envs.kinova_envs.KinovaPushCubeEnv import KinovaPushCubeEnv
 from envs.kinova_envs.KinovaPickCubeEnv import KinovaPickCubeEnv
+from envs.kinova_envs.KinovaStackCubeEnv import KinovaStackCubeEnv
 
 SUPPORTED_TASKS = [
-  "kinova_push_cube",  
-  "kinova_pick_cube"
+  "kinova_push_cube",
+  "kinova_pick_cube",
+  "kinova_stack_cube"
 ]
 
